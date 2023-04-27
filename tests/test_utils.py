803abdfa1291971b15d40e12b99c652504fa30af
pgleeson <p.gleeson@gmail.com>
2023-04-27 14:35:46 +0100

    Disable casting of floats in evaluate() to ints unless cast_to_int set

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,12 +19,16 @@ class TestUtils(unittest.TestCase):
         params = {"p": 33}
         assert evaluate("p+p", params, verbose=True) == 66
 
+        print("======")
         assert type(evaluate("33")) == int
-        assert type(evaluate("33.0")) == int
-        assert type(evaluate("33.1")) == float
-        assert type(evaluate("33.1a")) == str
+        assert type(evaluate("33", cast_to_int=True)) == int
+        assert type(evaluate("33.0")) == float
+        assert type(evaluate("33.0", cast_to_int=True)) == int
 
-        assert type(evaluate("33.1a")) == str
+        assert type(evaluate("33.1")) == float
+        assert type(evaluate("33.1a", verbose=True)) == str
+
+        assert type(evaluate("a")) == str
 
         import random
 
@@ -40,6 +44,21 @@ class TestUtils(unittest.TestCase):
         params = {"a": np.array([0, 1, 0]), "b": np.array([1, 1, 3])}
 
         assert evaluate("a+b", params, verbose=True)[2] == 3
+
+        params = {"a1": np.array([1]), "b": np.array([1, 1, 3])}
+
+        a1_b = evaluate("a1+b", params, verbose=True)
+        assert a1_b[2] == 4
+
+        params = {"A": np.ones([2, 2]), "B": np.ones([2, 2])}
+
+        AplusB = evaluate("A+B", params, verbose=True)
+        assert AplusB[0, 0] == 2
+        assert AplusB.shape == (2, 2)
+
+        AtimesB = evaluate("A*B", params, verbose=True)
+        assert AtimesB[0, 0] == 1
+        assert AtimesB.shape == (2, 2)
 
     def test_val_info_tuple(self):
         print(_val_info((1, 2)))
